88ed8491afdb3beb14c94fd2a62d572a755da100
Anton Smorodskyi <asmorodskyi@suse.com>
2020-02-10 16:58:17 +0100

    Filter out noisy messages from log

--- logviewer.js
+++ logviewer.js
@@ -11,28 +11,39 @@ var logLines = document.body.innerText.split('\n');
 document.body.innerText = "";
 var lineDiv = null;
 var msgStyle = msgFontSize + "color:black;";
+var append = true;
 for (var i = 0; i < logLines.length; i++) {
-    var matched = logLines[i].match(/\[\d{4}-\d{2}-\d{2}T(\d{2}:\d{2}:\d{2}\.\d{3,4}) CET\] \[(debug|info|warn|error)\](.*)/);
+    var matched = logLines[i].match(/\[\d{4}-\d{2}-\d{2}T(\d{2}:\d{2}:\d{2}\.\d{3,4}) CET\] \[(debug|info|warn|error)\] (\[pid:\d{1,5}\])?(.*)/);
     if (matched) {
         if (lineDiv) {
             document.body.appendChild(lineDiv);
         }
+        var msg = matched[4];
         lineDiv = document.createElement("div");
-        switch (matched[2]) {
-            case "info":
-                msgStyle = msgFontSize + "color:green;";
-                break;
-            case "warn":
-                msgStyle = msgFontSize + "color:orange;";
-                break;
-            case "error":
-                msgStyle = msgFontSize + "color:red;";
-                break;
-            default:
-                msgStyle = msgFontSize + "color:black;";
-        };
-        lineDiv.innerHTML = createFontElement('[' + matched[1] + ']', timeStyle) + createFontElement(matched[3], msgStyle);
-    } else {
+        if (msg.match(/Download of .* processed:/)) {
+            append = false;
+            lineDiv.innerHTML = createFontElement('[' + matched[1] + ']', timeStyle) + createFontElement(msg, msgStyle);
+        } else if (msg.match(/Output of rsync:/) || msg.match(/waiting for screen change:/)) {
+            append = false;
+        }
+        else { // no match: (\d{1,2}\.\d)s, best candidate: ([\w-]* \([\d\.]*\))
+            append = true;
+            switch (matched[2]) {
+                case "info":
+                    msgStyle = msgFontSize + "color:green;";
+                    break;
+                case "warn":
+                    msgStyle = msgFontSize + "color:orange;";
+                    break;
+                case "error":
+                    msgStyle = msgFontSize + "color:red;";
+                    break;
+                default:
+                    msgStyle = msgFontSize + "color:black;";
+            };
+            lineDiv.innerHTML = createFontElement('[' + matched[1] + ']', timeStyle) + createFontElement(msg, msgStyle);
+        }
+    } else if (append) {
         lineDiv.innerHTML += "<br>" + createFontElement(logLines[i], msgStyle);
     }
 }
